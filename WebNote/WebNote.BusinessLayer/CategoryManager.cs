74e4bddf3329aaf37a95b4c73a802ee09b0a315d
Salih Şeker <salih_seker_35@hotmail.com>
2019-11-02 12:27:03 +0300

    Notların Listelenmesi düzenlemeleri. Catrgry seçimine bağlı notların listelemeleri.

--- WebNote/WebNote.BusinessLayer/CategoryManager.cs
+++ WebNote/WebNote.BusinessLayer/CategoryManager.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using WebNote.Entities;
 using WebNote.DataAccessLayer.EntityFramework;
-
+using System.Linq.Expressions;
 
 namespace WebNote.BusinessLayer
 {
@@ -16,6 +16,11 @@ namespace WebNote.BusinessLayer
         public List<Category> GetCategories()
         {
             return repo_category.List();
+        }
+
+        public Category Find(Expression<Func<Category, bool>> where)
+        {
+            return repo_category.Find(where);
         }
         
     }
--- WebNote/WebNote.WebApp/Controllers/HomeController.cs
+++ WebNote/WebNote.WebApp/Controllers/HomeController.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebNote.Entities;
+using WebNote.BusinessLayer;
+using System.Net;
 
 namespace WebNote.WebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private NoteManager noteManager = new NoteManager();
+        private CategoryManager categoryManager = new CategoryManager();
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View(noteManager.GetAllNote());
+        }
+
+        public ActionResult ByCategory(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Category cat = categoryManager.Find(x => x.Id == id);
+            if (cat == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View("Index" , cat.Notes.ToList());
         }
     }
 }

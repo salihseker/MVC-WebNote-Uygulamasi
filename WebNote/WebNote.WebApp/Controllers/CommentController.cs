45a7bb0e528bdcc9717ee9290a15151fb1969dfa
Salih Şeker <salih_seker_35@hotmail.com>
2020-01-15 00:28:20 +0300

    Yorum İşlemleri - ModalPopup Ekleme

---
+++ WebNote/WebNote.WebApp/Controllers/CommentController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebNote.BusinessLayer;
+using WebNote.Entities;
+
+namespace WebNote.WebApp.Controllers
+{
+    public class CommentController : Controller
+    {
+        private NoteManager noteManager = new NoteManager();
+        private CommentManager commentManager = new CommentManager();
+
+        public ActionResult ShowNoteComments(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            //Note note = noteManager.Find(x => x.Id == id);
+            Note note = noteManager.ListQueryable().Include("Comments").FirstOrDefault(x => x.Id == id);
+
+            if (note == null)
+            {
+                return HttpNotFound();
+            }
+
+            return PartialView("_PartialComments", note.Comments.ToList());
+        }
+    }
+}

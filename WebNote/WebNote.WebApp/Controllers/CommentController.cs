31317c7180f70eea02084885aa5119c44bb764b1
Salih Şeker <salih_seker_35@hotmail.com>
2020-01-15 23:03:09 +0300

    Yorum İşlemleri -- Düzenleme ,Silme , Ekleme

--- WebNote/WebNote.WebApp/Controllers/CommentController.cs
+++ WebNote/WebNote.WebApp/Controllers/CommentController.cs
@@ -7,6 +7,7 @@ using System.Web;
 using System.Web.Mvc;
 using WebNote.BusinessLayer;
 using WebNote.Entities;
+using WebNote.WebApp.Models;
 
 namespace WebNote.WebApp.Controllers
 {
@@ -31,6 +32,86 @@ namespace WebNote.WebApp.Controllers
             }
 
             return PartialView("_PartialComments", note.Comments.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int? id, string text)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Comment comment = commentManager.Find(x => x.Id == id);
+
+            if (comment == null)
+            {
+                return new HttpNotFoundResult();
+            }
+
+            comment.Text = text;
+
+            if (commentManager.Update(comment) > 0)
+            {
+                return Json(new { result = true }, JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(new { result = false }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Comment comment = commentManager.Find(x => x.Id == id);
+
+            if (comment == null)
+            {
+                return new HttpNotFoundResult();
+            }
+
+            if (commentManager.Delete(comment) > 0)
+            {
+                return Json(new { result = true }, JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(new { result = false }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Comment comment, int? noteid)
+        {
+            ModelState.Remove("CreatedOn");
+            ModelState.Remove("ModifiedOn");
+            ModelState.Remove("ModifiedUsername");
+
+            if (ModelState.IsValid)
+            {
+                if (noteid == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+
+                Note note = noteManager.Find(x => x.Id == noteid);
+
+                if (note == null)
+                {
+                    return new HttpNotFoundResult();
+                }
+
+                comment.Note = note;
+                comment.Owner = CurrentSession.User;
+
+                if (commentManager.Insert(comment) > 0)
+                {
+                    return Json(new { result = true }, JsonRequestBehavior.AllowGet);
+                }
+            }
+
+            return Json(new { result = false }, JsonRequestBehavior.AllowGet);
         }
     }
 }
